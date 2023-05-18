ecef111b9d22ca5224ae8a8f841454c65f6b1324
Ollllesya <112069824+Ollllesya@users.noreply.github.com>
2023-05-18 21:17:48 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+http.createServer(function(request,response){
+
+	response.setHeader("Content-Type","text/html; charset=utf-8;");
+
+	if(request.url === "/" || request.url === "/"){
+		response.write('<h2>Home Page</h2>');
+	}
+
+	else if(request.url === "/is-13"){
+		response.write('<h2> Olesia Skydan 2 cours IS-13</h2>');
+	}
+	else{
+		response.write('<h2>Not found</h2>');
+	}
+	response.end();
+}).listen(3000);
